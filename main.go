4f7644306ca44a797c51bc08e70cd12cc050397d
Lisandra Melo <mendie73@gmail.com>
2022-12-20 02:34:52 -0300

    Pontos e serviçoa funcionais

--- main.go
+++ main.go
@@ -1,110 +1,76 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
 	"time"
 )
 
-func server(c chan int) {
+func server(c chan int, is_serve bool) {
 	source := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(source)
 	serve_result := r.Intn(2)
-	println("First serve:", serve_result)
-	if serve_result == 0 {
-		println("First fault!")
-		rand.Seed(time.Now().UnixNano())
-		serve_result = r.Intn(2)
-		println("Second serve:", serve_result)
+	if is_serve {
+		println("First serve:", serve_result)
 		if serve_result == 0 {
-			println("Double Fault! Point to reciever.")
+			println("First fault!")
+			rand.Seed(time.Now().UnixNano())
+			serve_result = r.Intn(2)
+			println("Second serve:", serve_result)
+			if serve_result == 0 {
+				println("Double Fault! Point to reciever.")
+			}
+		}
+	} else {
+		println("Server:", serve_result)
+		if serve_result == 0 {
+			println("Server missed.")
 		}
 	}
+
 	c <- serve_result
 }
 
-func receiver(c chan int) {
+func receiver(c chan int, is_serve bool) {
 	source := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(source)
 	bola_server := <-c
-	println(bola_server)
 	if bola_server == 0 {
 		println("Ending point.")
 		close(c)
 	} else {
 		receiver_ball := r.Intn(2)
 		println("Receiver", receiver_ball)
-		if receiver_ball == 0 {
-			println("Ace point!")
-			close(c)
+		if is_serve {
+			if receiver_ball == 0 {
+				println("Ace point!")
+				close(c)
+			} else {
+				c <- receiver_ball
+			}
 		} else {
-			c <- receiver_ball
+			if receiver_ball == 0 {
+				println("Receiver missed. Point server.")
+				close(c)
+			} else {
+				c <- receiver_ball
+			}
 		}
-	}
-}
 
-func first(c chan int) {
-	source := rand.NewSource(time.Now().UnixNano())
-	r := rand.New(source)
-	bola_receiver := <-c
-	println(bola_receiver)
-	if bola_receiver == 0 {
-		println("Ending point.")
-		close(c)
-	} else {
-		server_ball := r.Intn(2)
-		println("Server", server_ball)
-		if server_ball == 0 {
-			println("Server missed")
-			c <- server_ball
-		} else {
-			println("Server returns ball. Point continues")
-			c <- server_ball
-		}
-	}
-}
-
-func second(c chan int) {
-	source := rand.NewSource(time.Now().UnixNano())
-	r := rand.New(source)
-	bola_server := <-c
-	println(bola_server)
-	if bola_server == 0 {
-		println("Ending point.")
-		close(c)
-	} else {
-		receiver_ball := r.Intn(2)
-		println("Receiver", receiver_ball)
-		if receiver_ball == 0 {
-			println("Receiver missed")
-			c <- receiver_ball
-		} else {
-			println("Receiver returns ball. Point continues")
-			c <- receiver_ball
-		}
 	}
 }
 
 func main() {
 
 	quadra := make(chan int)
+	status := true
+	serve := true
 
-	go server(quadra)
-	time.Sleep(1 * time.Second)
-	go receiver(quadra)
-	time.Sleep(1 * time.Second)
-	_, status := <-quadra
-
-	if status {
-		fmt.Println("Point continues!")
-		for status {
-			go first(quadra)
-			time.Sleep(1 * time.Second)
-			go second(quadra)
-			time.Sleep(1 * time.Second)
-			_, status = <-quadra
-		}
-	} else {
-		fmt.Println("Point ends!")
+	for status {
+		go server(quadra, serve)
+		time.Sleep(1 * time.Second)
+		go receiver(quadra, serve)
+		time.Sleep(1 * time.Second)
+		_, status = <-quadra
+		serve = false
 	}
 }
